b3cedbac680cc2ea0a6c474164f3eb632849a0b9
Aryan Khurana <aryankhurana1511@gmail.com>
2024-09-12 15:46:37 -0400

    Add practice python code

---
+++ 00-Python-Refresher/Modules and Packages/funcs/more_funcs/more_test.py
@@ -0,0 +1,5 @@
+import utility
+
+def more_print():
+    print(utility.multiply(2, 3))
+    print("Longer printer")
---
+++ 00-Python-Refresher/Modules and Packages/main.py
@@ -0,0 +1,7 @@
+import utility
+import funcs.test
+import funcs.more_funcs.more_test
+
+print(utility.multiply(2, 3))
+print(funcs.test.printer())
+print(funcs.more_funcs.more_test.more_print())
